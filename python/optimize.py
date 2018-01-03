163aa11cadb18454143b6f421fb61a84bcbb3a02
gdyshi <gdyshi@126.com>
2018-01-03 13:13:17 +0800

    1

--- python/optimize.py
+++ python/optimize.py
@@ -9,29 +9,41 @@ import tensorflow as tf
 import numpy as np
 
 FLAGS = None
-BATCH_MINI = 50  # mini-batch
-
-LR_MOMENTUM = 'monentum'
-LR_NAG = 'monentum'
-LR_ADAGRAD = 'monentum'
-LR_RMSPROP = 'RMSprop'
-LR_ADAM = 'Adam'
-LR_ADADELTA = 'monentum'
-
-LR_DECAY_LINER = 'LINER' # 线性下降
-LR_DECAY_LINER = 'LINER' # 指数下降
-
 
 layers = [784, 270, 90, 30, 10]
+TRAINING_STEPS = 20000
+LR_NONE = 'none'
+LR_MOMENTUM = 'monentum'
+LR_NAG = 'nag'
+LR_ADAGRAD = 'adagrad'
+LR_RMSPROP = 'RMSprop'
+LR_ADAM = 'Adam'
+LR_ADADELTA = 'adadelta'
 
-# 激活函数选择
-ACTIVATION_FUNCTION = ACTIVATION_FUNCTION_TANH
+LR_DECAY_LINER = 'LINER'  # 线性下降
+LR_DECAY_EXP = 'EXP'  # 指数下降
+LR_DECAY_NATURAL_EXP = 'NATURAL_EXP'  # e为底的指数下降
+# LR_DECAY_polynomial = 'polynomial'  # 多项式
+
+# 0.02
+# LINER、EXP、NATURAL_EXP:0.2
+# ADAM:0.001
+learning_rate = 0.02
+batch_size = 50  # mini-batch
+
+# OPTIMIZE_METHORD = LR_ADAM
+OPTIMIZE_METHORD = LR_ADAM
+
+
+def accuracy(y_pred, y_real):
+    correct_prediction = tf.equal(tf.argmax(y_pred, 1), tf.argmax(y_real, 1))
+    acc = tf.reduce_mean(tf.cast(correct_prediction, tf.float32))
+    return acc
 
 
 def main(_):
     # Import data
     mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-    print(layers)
     # Create the model
     x = tf.placeholder(tf.float32, [None, 784])
     for i in range(0, len(layers) - 1):
@@ -39,49 +51,64 @@ def main(_):
 
         node_in = layers[i]
         node_out = layers[i + 1]
-        if INIT_METHORD == INIT_METHORD_ZERO:
-            W = tf.Variable(tf.zeros([node_in, node_out]))
-        elif INIT_METHORD == INIT_METHORD_RANDOM:
-            W = tf.Variable(np.random.randn(node_in, node_out).astype('float32'))
-        elif INIT_METHORD == INIT_METHORD_LITTLE_RANDOM:
-            W = tf.Variable(np.random.randn(node_in, node_out).astype('float32') * 0.01)
-        elif INIT_METHORD == INIT_METHORD_XAVIER:
-            W = tf.Variable(np.random.randn(node_in, node_out).astype('float32') / (np.sqrt(node_in)))
-        elif INIT_METHORD == INIT_METHORD_HE:
-            W = tf.Variable(np.random.randn(node_in, node_out).astype('float32') / (np.sqrt(node_in / 2)))
+        W = tf.Variable(np.random.randn(node_in, node_out).astype('float32') / (np.sqrt(node_in)))
         b = tf.Variable(np.random.randn(node_out).astype('float32'))
         z = tf.matmul(X, W) + b
-
-        if BATCH_NORM:
-            z = tf.contrib.layers.batch_norm(z, center=True, scale=True,
-                                             is_training=True)
-
-        if ACTIVATION_FUNCTION == ACTIVATION_FUNCTION_TANH:
-            y = tf.nn.tanh(z)
-        elif ACTIVATION_FUNCTION == ACTIVATION_FUNCTION_SIGMOID:
-            y = tf.nn.sigmoid(z)
-        elif ACTIVATION_FUNCTION == ACTIVATION_FUNCTION_RELU:
-            y = tf.nn.relu(z)
+        y = tf.nn.tanh(z)
 
     # Define loss and optimizer
     y_ = tf.placeholder(tf.float32, [None, 10])
-    print(y)
     loss = tf.reduce_mean(tf.norm(y_ - y, axis=1) ** 2) / 2
-    train_step = tf.train.GradientDescentOptimizer(3.0).minimize(loss)
-    # train_step = tf.train.GradientDescentOptimizer(3.0 / 256).minimize(loss)  # relu 16 tanh 16
 
-    correct_prediction = tf.equal(tf.argmax(y, 1), tf.argmax(y_, 1))
-    accuracy = tf.reduce_sum(tf.cast(correct_prediction, tf.int32))
+    global_step = tf.Variable(0, trainable=False)
+    if OPTIMIZE_METHORD == LR_DECAY_EXP:
+        # decayed_learning_rate = learning_rate * decay_rate ^ (global_step / decay_steps)
+        decayed_learning_rate = tf.train.exponential_decay(learning_rate, global_step, 1000, 0.96, staircase=True)
+    elif OPTIMIZE_METHORD == LR_DECAY_NATURAL_EXP:
+        # decayed_learning_rate = learning_rate * exp(-decay_rate * global_step)
+        decayed_learning_rate = tf.train.inverse_time_decay(learning_rate, global_step, 1000, 0.96, staircase=True)
+    elif OPTIMIZE_METHORD == LR_DECAY_LINER:
+        # decayed_learning_rate = learning_rate / (1 + decay_rate * t)
+        decayed_learning_rate = tf.train.inverse_time_decay(learning_rate, global_step, 1000, 0.96, staircase=True)
+    # elif OPTIMIZE_METHORD == LR_DECAY_polynomial:
+    #     # global_step = min(global_step, decay_steps)
+    #     # decayed_learning_rate = (learning_rate - end_learning_rate) *
+    #     # (1 - global_step / decay_steps) ^ (power) +
+    #     # end_learning_rate
+    #     decayed_learning_rate = tf.train.polynomial_decay(learning_rate, global_step, 100, 50)
+    else:
+        decayed_learning_rate = learning_rate
+
+    if OPTIMIZE_METHORD == LR_MOMENTUM:
+        train_step = tf.train.MomentumOptimizer(decayed_learning_rate, 0.5).minimize(loss, global_step=global_step)
+    elif OPTIMIZE_METHORD == LR_NAG:
+        train_step = tf.train.MomentumOptimizer(decayed_learning_rate, 0.5, use_nesterov=True).minimize(loss,
+                                                                                                        global_step=global_step)
+    elif OPTIMIZE_METHORD == LR_ADAGRAD:
+        train_step = tf.train.AdagradOptimizer(decayed_learning_rate).minimize(loss, global_step=global_step)
+    elif OPTIMIZE_METHORD == LR_RMSPROP:
+        train_step = tf.train.RMSPropOptimizer(decayed_learning_rate).minimize(loss, global_step=global_step)
+    elif OPTIMIZE_METHORD == LR_ADAM:
+        train_step = tf.train.AdamOptimizer(decayed_learning_rate).minimize(loss, global_step=global_step)
+    elif OPTIMIZE_METHORD == LR_ADADELTA:
+        train_step = tf.train.AdadeltaOptimizer(decayed_learning_rate).minimize(loss, global_step=global_step)
+    else:
+        train_step = tf.train.GradientDescentOptimizer(decayed_learning_rate).minimize(loss, global_step=global_step)
+
+    acc = accuracy(y, y_)
 
     with tf.Session() as sess:
         sess.run(tf.global_variables_initializer())
-        for i in range(20000):
-            batch_xs, batch_ys = mnist.train.next_batch(10)
+        for i in range(TRAINING_STEPS):
+            batch_xs, batch_ys = mnist.train.next_batch(batch_size)
             if i % 1000 == 0:
-                train_accuracy = accuracy.eval(feed_dict={x: mnist.test.images,
-                                                          y_: mnist.test.labels})
-                print('step %d, training accuracy %g' % (i, train_accuracy))
+                valid_acc = acc.eval(feed_dict={x: mnist.validation.images,
+                                                     y_: mnist.validation.labels})
+                print("After %d training step(s), accuracy on validation is %g." % (i, valid_acc))
             train_step.run(feed_dict={x: batch_xs, y_: batch_ys})
+        test_acc = acc.eval(feed_dict={x: mnist.test.images,
+                                         y_: mnist.test.labels})
+        print("After %d training step(s), accuracy on test is %g." % (TRAINING_STEPS, test_acc))
 
 
 if __name__ == '__main__':
